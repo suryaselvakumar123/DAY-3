2b02610c7be0b4521af7cfae50b30d85af7e257e
suryaselvakumar123 <suryaselvakumar1007@gmail.com>
2023-01-24 16:42:06 +0530

    commit all

---
+++ DAY 3/resume.js
@@ -0,0 +1,29 @@
+let resume=[
+    {"basics": [
+     {
+      "name": "SURYA.S",
+      "email": "suryaselvakumar1007@gamil.com",
+      "phone": 6382892102,
+      "degree": "B.E",
+    "location": "India"
+    }
+],
+    "education": [
+      {
+        "institution": "Bannari amman institute of Technology",
+        "department": "Mechanical Engineering ",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "gpa": 8.06,
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ]
+}
+]
+  console.log(resume);
+
